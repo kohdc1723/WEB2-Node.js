7f0b07ea703ef5c01d3b864c42846570eaf8e9ae
kohdc1723 <root@Dongchan-Koh-MacBookAir.local>
2022-05-07 01:09:10 -0700

    Home page implemented

--- main.js
+++ main.js
@@ -6,11 +6,12 @@ const app = http.createServer((request, response) => {
     let varURL = request.url;
     let queryData = url.parse(varURL, true).query;
     let pathName = url.parse(varURL, true).pathname;
-    let title = queryData.id;
-    
+        
     if (pathName === "/") {
-        fs.readFile(`./data/${queryData.id}`, 'utf8', (error, description) => {
-            var template = `
+        if (queryData.id === undefined) {
+            let title = "Welcome";
+            let description = "Hello, Node.js";
+            let template = `
                 <!doctype html>
                 <html>
                     <head>
@@ -31,11 +32,37 @@ const app = http.createServer((request, response) => {
             `;
             response.writeHead(200);
             response.end(template);
-        });
+        } else {
+            fs.readFile(`./data/${queryData.id}`, 'utf8', (error, data) => {
+                let title = queryData.id;
+                let description = data;
+                let template = `
+                    <!doctype html>
+                    <html>
+                        <head>
+                            <title>WEB1 - ${title}</title>
+                            <meta charset="utf-8">
+                        </head>
+                        <body>
+                            <h1><a href="/">WEB</a></h1>
+                            <ol>
+                                <li><a href="/?id=HTML">HTML</a></li>
+                                <li><a href="/?id=CSS">CSS</a></li>
+                                <li><a href="/?id=JavaScript">JavaScript</a></li>
+                            </ol>
+                            <h2>${title}</h2>
+                            <p>${description}</p>
+                        </body>
+                    </html>
+                `;
+                response.writeHead(200);
+                response.end(template);
+            });
+        }
     } else {
         response.writeHead(404);
         response.end('Not found');
     }
 });
 
-app.listen(7000);
+app.listen(3000);
